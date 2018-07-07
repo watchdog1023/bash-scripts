4cd848fc87c9d9f72382f5989353d73065f22d10
watchdog1023 <Naickercd@gmail.com>
2018-07-07 14:02:27 +0200

    Delete get smfl-pi.sh

--- PI/get smfl-pi.sh
+++
@@ -1,8 +0,0 @@
-git clone https://github.com/mickelson/sfml-pi.git
-sudo apt-get install cmake libflac-dev libogg-dev libvorbis-dev libopenal-dev libjpeg8-dev libfreetype6-dev libudev-dev libraspberrypi-dev
-cd smf*
-mkdir build 
-cd build
-cmake .. -DSFML_RPI=1 -DEGL_INCLUDE_DIR=/opt/vc/include -DEGL_LIBRARY=/opt/vc/lib/libbrcmEGL.so -DGLES_INCLUDE_DIR=/opt/vc/include -DGLES_LIBRARY=/opt/vc/lib/libbrcmGLESv2.so
-sudo make install
-sudo ldconfig
