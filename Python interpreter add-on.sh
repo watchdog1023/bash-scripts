a8416736748dd87d39fbb5a6e492db618332b18c
watchdog1023 <Naickercd@gmail.com>
2018-11-21 10:09:42 +0200

    Update Python interpreter add-on.sh

--- Python interpreter add-on.sh
+++ Python interpreter add-on.sh
@@ -1,106 +1,112 @@
-REM Python 2
-pip install opencv
-pip install Scrapy
-pip install wxPython
-pip install Pillow
-pip install SQLAlchemy
-pip install BeautifulSoup
-pip install Twisted
-pip install NumPy
-pip install SciPy
-pip install matplotlib
-pip install Pygame
-pip install Pyglet
-pip install pyQT
-pip install pyGtk
-pip install Scapy
-pip install pywin32
-pip install nltk
-pip install nose
-pip install SymPy
-pip install IPython
-pip install pyUSB
-pip install pyGoogle
-pip install USPP
-pip install pySerial
-pip install geoname.py
-pip install VideoCapture
-pip install Pygame
-pip install Pyglet
-pip install PySoy
-pip install Zappa
-pip install pyOpenGL
-pip install FMOD
-pip install pyS60
-pip install VPython
-pip install Sanic
-pip install uvloop
-pip install asyncpg
-pip install boto3
-pip install gym
-pip install universe
-pip install Bokeh
-pip install Blaze
-pip install arrow
-pip install hug
-pip install FlaskFabric
-pip install flask
-pip install nose
-pip install pyglet
-pip install Pandas
-pip install tflearn
-pip install --upgrade tensorflow
-pip install pycryto
-REM Python 3
-pip3 install pycryto
-pip3 install --upgrade tensorflow
-pip3 install tflearn
-pip3 install Scrapy
-pip3 install wxPython
-pip3 install Pillow
-pip3 install SQLAlchemy
-pip3 install BeautifulSoup
-pip3 install Twisted
-pip3 install NumPy
-pip3 install SciPy
-pip3 install matplotlib
-pip3 install Pygame
-pip3 install Pyglet
-pip3 install pyQT
-pip3 install pyGtk
-pip3 install Scapy
-pip3 install pywin32
-pip3 install nltk
-pip3 install nose
-pip3 install SymPy
-pip3 install IPython
-pip3 install pyUSB
-pip3 install pyGoogle
-pip3 install USPP
-pip3 install pySerial
-pip3 install geoname.py
-pip3 install VideoCapture
-pip3 install Pygame
-pip3 install Pyglet
-pip3 install PySoy
-pip3 install Zappa
-pip3 install pyOpenGL
-pip3 install FMOD
-pip3 install pyS60
-pip3 install VPython
-pip3 install Sanic
-pip3 install uvloop
-pip3 install asyncpg
-pip3 install boto3
-pip3 install gym
-pip3 install universe
-pip3 install Bokeh
-pip3 install Blaze
-pip3 install arrow
-pip3 install hug
-pip3 install FlaskFabric
-pip3 install flask
-pip3 install nose
-pip3 install pyglet
-pip3 install Pandas
-pip3 install opencv
+#!/bin/sh
+#Python 2
+if [  -e /usr/bin/pip2 ];then
+	pip2 install --upgrade pip
+	pip2 install opencv
+	pip2 install Scrapy
+	pip2 install wxPython
+	pip2 install Pillow
+	pip2 install SQLAlchemy
+	pip2 install BeautifulSoup
+	pip2 install Twisted
+	pip2 install NumPy
+	pip2 install SciPy
+	pip2 install matplotlib
+	pip2 install Pygame
+	pip2 install Pyglet
+	pip2 install pyQT
+	pip2 install pyGtk
+	pip2 install Scapy
+	pip2 install pywin32
+	pip2 install nltk
+	pip2 install nose
+	pip2 install SymPy
+	pip2 install IPython
+	pip2 install pyUSB
+	#pip2 install pyGoogle
+	pip2 install USPP
+	pip2 install pySerial
+	pip2 install geoname.py
+	pip2 install VideoCapture
+	pip2 install Pygame
+	pip2 install Pyglet
+	pip2 install PySoy
+	pip2 install Zappa
+	pip2 install pyOpenGL
+	pip2 install FMOD
+	pip2 install pyS60
+	pip2 install VPython
+	pip2 install Sanic
+	pip2 install uvloop
+	pip2 install asyncpg
+	pip2 install boto3
+	pip2 install gym
+	pip2 install universe
+	pip2 install Bokeh
+	pip2 install Blaze
+	pip2 install arrow
+	pip2 install hug
+	pip2 install FlaskFabric
+	pip2 install flask
+	pip2 install nose
+	pip2 install pyglet
+	pip2 install Pandas
+	pip2 install tflearn
+	pip2 install --upgrade tensorflow
+	pip2 install pycryto
+fi
+#Python 3
+if [  -e /usr/bin/pip3 ];then
+	pip3 install pycryto
+	pip3 install --upgrade tensorflow
+	pip3 install tflearn
+	pip3 install Scrapy
+	pip3 install wxPython
+	pip3 install Pillow
+	pip3 install SQLAlchemy
+	pip3 install BeautifulSoup
+	pip3 install Twisted
+	pip3 install NumPy
+	pip3 install SciPy
+	pip3 install matplotlib
+	pip3 install Pygame
+	pip3 install Pyglet
+	pip3 install pyQT
+	pip3 install pyGtk
+	pip3 install Scapy
+	pip3 install pywin32
+	pip3 install nltk
+	pip3 install nose
+	pip3 install SymPy
+	pip3 install IPython
+	pip3 install pyUSB
+	#pip3 install pyGoogle
+	pip3 install USPP
+	pip3 install pySerial
+	pip3 install geoname.py
+	pip3 install VideoCapture
+	pip3 install Pygame
+	pip3 install Pyglet
+	pip3 install PySoy
+	pip3 install Zappa
+	pip3 install pyOpenGL
+	pip3 install FMOD
+	pip3 install pyS60
+	pip3 install VPython
+	pip3 install Sanic
+	pip3 install uvloop
+	pip3 install asyncpg
+	pip3 install boto3
+	pip3 install gym
+	pip3 install universe
+	pip3 install Bokeh
+	pip3 install Blaze
+	pip3 install arrow
+	pip3 install hug
+	pip3 install FlaskFabric
+	pip3 install flask
+	pip3 install nose
+	pip3 install pyglet
+	pip3 install Pandas
+	pip3 install opencv
+fi
